3c7e48f5977a40711f85b366d857caae53b82cb8
marlonlange <marlonlange@live.de>
2017-05-02 15:52:04 +0200

    Initiales einchecken

---
+++ SQLTestClient/SQLTestClient/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLTestClient
+{
+    public class MainViewModel
+    {
+        /// <summary>
+        /// Standard-Konstruktor
+        /// </summary>
+        public MainViewModel()
+        {
+            
+        }
+
+        /// <summary>
+        /// Gibt die angegebene Log-Information in der RichTextBox aus.
+        /// </summary>
+        /// <param name="text">Information, die ausgegeben werden soll</param>
+        public void writeOutput(string text)
+        {
+            
+        }
+    }
+}
